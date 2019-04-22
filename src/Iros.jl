d701a1b8de7fd914a4df7fa469e7ac1e87dd40e4
William R Sweeney <wrsweeney2@gmail.com>
2019-04-22 14:28:11 -0400

    Init

---
+++ src/Iros.jl
@@ -0,0 +1,12 @@
+module Iros
+
+using Reexport
+
+@reexport using IrosBase
+@reexport using IrosSpectral
+# @reexport using IrisFloquet
+# @reexport using IrisScattering
+# @reexport using IrisSALT
+# @reexport using IrisSCPA
+
+end # module
