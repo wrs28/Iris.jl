0cb39eb9654b04b50a6f9d3eabcec0ee78aa35f0
William R Sweeney <wrsweeney2@gmail.com>
2019-04-30 00:12:21 -0400

    IrosDefaults

--- src/Iros.jl
+++ src/Iros.jl
@@ -1,7 +1,22 @@
+"""
+    module Iros
+
+A package for scalar finite difference resonance and scattering calculations, for both
+    closed and open systems.
+    * `IrosDefaults`: contains all default parameters in one location for uniformity and easy alteration.
+    * `IrosBase`: the essential structures used to define a simulation and to construct the necessary operators
+    * `IrosSpectral`: linear and non-linear eigenvalue problems for resonant, anti-resonant, and RSM problems,
+        for closed systems, or for open systems via boundary matching or PMLs.
+    * `IrosFloquet`: for finding the band structures and dispersion curves of crystal structures
+    * `IrosScattering`: for solving inhomogeneous scattering problems
+    * `IrosSALT`: the SALT algorithm for lasing
+    * `IrosSCPA`: saturable CPA
+"""
 module Iros
 
 using Reexport
 
+using IrosDefaults
 @reexport using IrosBase
 @reexport using IrosSpectral
 # @reexport using IrisFloquet
