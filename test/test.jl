e97f6825039f5252c8e4285603943968c608170c
William R Sweeney <wrsweeney2@gmail.com>
2019-07-02 17:43:37 -0400

    Iris

--- test/test.jl
+++ test/test.jl
@@ -1,66 +1,42 @@
-using Revise, Plots
-using Iros
+using Iris
+using SpecialFunctions
 
 c = Circle(1,2,3)
-plot(c)
-e = Ellipse(1,2,3,4,.1)
-plot(e)
-d = DeformedDisk{2}(1,2,3,[4,5],[.06,.07],[8,9],.1)
-plot(d)
-
-a = Annulus(1,2,3,4,5)
-plot(a)
-
-s = Square(1,2,3,.1)
-plot(s)
+lat = Lattices.Cartesian(1/21,1/21)
+bnd = Boundary(c,DirichletBC())
+sim = Simulation(Cavity(bnd,lat))
+k,ψ = eig_kl(sim,eps(),nev=20)
+@testset "Zeros of Bessel J" begin
+    @test abs(besselj(0,k[1])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(1,k[2])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(1,k[3])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(2,k[4])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(2,k[5])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(0,k[6])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(3,k[7])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(3,k[8])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(1,k[9])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(1,k[10])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(4,k[11])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(4,k[12])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(2,k[13])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(2,k[14])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(0,k[15])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(5,k[16])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(5,k[17])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(3,k[18])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(3,k[19])) ≤ max(lat.dx,lat.dy)
+    @test abs(besselj(6,k[20])) ≤ max(lat.dx,lat.dy)
+end
 
 r = Rectangle(1,2,3,4,.1)
-plot(r)
-
-lat = Lattices.Cartesian(1/11,1/11)
-
-DirichletBC()
-
-bnd = Boundary(DeformedDisk{2}(1,1,2,[2,3],[.5,.1],[0,0]),DirichletBC())
-
-dom = Cavity(bnd,lat)
-
-sim = Simulation(dom)
-
-# bnd = Boundary(Rectangle(1,.1,0,0,0.1),DirichletBC())
-lat = Lattices.Cartesian(1/101,1/101,.1)
-dom = Cavity(bnd,lat)
-sim = Simulation(dom)
-plot(sim)
-
-k,ψ = eig_kl(sim,5,nev=25)
-
-
-n=0
-n+=1;print(n,": ");println(k[n]);plot(sim,ψ,n,by=abs2)
-    # plot!(bnd)
-
-plot(xlims=(0.4,0.5),ylims=(2.7,2.8))
-    # plot!(sim,ψ,3,by=abs2,ms=5)
-    scatter!(sim.x[sim.surface],sim.y[sim.surface],label="Surface")
-    # scatter!(sim.x[sim.exterior],sim.y[sim.exterior],label="Exterior")
-    indp = ind+1
-    inds = findall(sim.link_y_bool.&(sim.link_start.==indp))
-    scatter!(sim.x[[indp]],sim.y[[indp]],label="point",shape=:star,ms=10)
-    scatter!(sim.x[sim.link_stop[inds]],sim.y[sim.link_stop[inds]],marker_z=sim.link_weight[inds]*lat.dx*lat.dy,color=:diverging)
-    plot!(clims=(-1,1))
-    plot!(bnd,label=:bnd)
-    plot!(leg=true)
-
-
-
-plot!(sim,ψ,6,by=abs2,ms=5)
-    # scatter(sim.x[sim.interior],sim.y[sim.interior])
-scatter!(sim.x[sim.surface],sim.y[sim.surface])
-scatter!(sim.x[sim.exterior],sim.y[sim.exterior])
-    # scatter!(sim.x[sim.bulk],sim.y[sim.bulk])
-    scatter!(sim.x[sim.corner],sim.y[sim.corner])
-    # plot!(sim.tessellation)
-    # plot!(bnd.shape)
-
-    scatter!(sim.x[sim.lattice_wall],sim.y[sim.lattice_wall])
+bnd = Boundary(r,NeumannBC())
+sim = Simulation(Cavity(bnd,lat))
+k,ψ = eig_kl(sim,eps(),nev=5)
+@testset "1:2 Rectangle" begin
+    @test abs((k[1])^2 - (0π/1)^2 - (0π/2)^2) ≤ 100*2*max(lat.dx,lat.dy)^2
+    @test abs((k[2])^2 - (0π/1)^2 - (1π/2)^2) ≤ 100*2*max(lat.dx,lat.dy)^2
+    @test abs((k[3])^2 - (0π/1)^2 - (2π/2)^2) ≤ 100*2*max(lat.dx,lat.dy)^2
+    @test abs((k[4])^2 - (1π/1)^2 - (0π/2)^2) ≤ 100*2*max(lat.dx,lat.dy)^2
+    @test abs((k[5])^2 - (1π/1)^2 - (1π/2)^2) ≤ 100*2*max(lat.dx,lat.dy)^2
+end
